13b4a84e84f0ff9d5ede764bf648334a52a6602b
İlkin <97682974+ilkinsdqv@users.noreply.github.com>
2026-09-04 21:33:27 +0400

    feat(config): centralize gateway configuration

--- auth-service/src/main/java/az/texnoera/bank/authservice/client/UserClient.java
+++ auth-service/src/main/java/az/texnoera/bank/authservice/client/UserClient.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.UUID;
 
-@FeignClient(
-        name = "user-service",
-        url = "${clients.user-service.url}"
-)
+@FeignClient(name = "user-service")
 public interface UserClient {
 
     @GetMapping("/api/v1/users/authentication")
