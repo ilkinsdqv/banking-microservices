816504c93cf5107abc61f1417a2c7cc935222e12
İlkin <97682974+ilkinsdqv@users.noreply.github.com>
2026-09-11 20:44:20 +0400

    feat(transaction): add security and gateway integration

---
+++ transaction-service/src/main/java/az/texnoera/bank/transactionservice/transaction/controller/TransactionController.java
@@ -0,0 +1,60 @@
+package az.texnoera.bank.transactionservice.transaction.controller;
+
+import az.texnoera.bank.transactionservice.transaction.dto.request.CreateTransactionRequest;
+import az.texnoera.bank.transactionservice.transaction.dto.response.TransactionResponse;
+import az.texnoera.bank.transactionservice.transaction.service.TransactionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<TransactionResponse> createTransaction(
+            Authentication authentication,
+            @Valid @RequestBody CreateTransactionRequest request
+    ) {
+        UUID userId = (UUID) authentication.getPrincipal();
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(
+                        transactionService.createTransaction(
+                                userId,
+                                request
+                        )
+                );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TransactionResponse> getTransactionById(
+            @PathVariable UUID id
+    ) {
+        return ResponseEntity.ok(
+                transactionService.getTransactionById(id)
+        );
+    }
+
+    @GetMapping("/account/{accountId}")
+    public ResponseEntity<List<TransactionResponse>>
+    getTransactionsByAccountId(
+            @PathVariable UUID accountId
+    ) {
+        return ResponseEntity.ok(
+                transactionService.getTransactionsByAccountId(
+                        accountId
+                )
+        );
+    }
+}
